0733cafd7b35efb3ce791c0421aeaaf97095b702
Aaron Dean <adean30462@gmail.com>
2023-07-10 14:26:16 -0400

    chap 3 done

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        public List<Post> GetAll();
+        public Post GetById(int id);
+        public void Add(Post post);
+        public void Update(Post post);
+        public void Delete(int id);
+        public List<Post> GetAllWithComments();
+        public Post GetPostByIdWithComments(int id);
+    }
+}
